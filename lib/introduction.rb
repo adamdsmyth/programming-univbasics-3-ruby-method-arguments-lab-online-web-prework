f7978eea14da5f7c1265ea859f72de0dfca04a9b
merry-attribute-0729 <adamdsmyth@gmail.com>
2019-10-26 16:31:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,9 @@
 def introduction (name) 
   print "Hi, my name is #{name}.\n" 
 end
-
+def introduction_with_language (name, language)
+  print "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
